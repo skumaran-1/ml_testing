6bbbe49ecebc8673533f318fc90bd404a5846ab1
Siddharth Kumaran <skumaran@rivasolutionsinc.com>
2019-07-30 10:12:48 -0400

    test

---
+++ ml_testing.py
@@ -0,0 +1,6 @@
+import numpy as np                                  #for large and multi-dimensional arrays
+import pandas as pd                                 #for data manipulation and analysis
+
+data = pd.read_csv(r'ITMO_raw_data.csv')
+
+print(data)
