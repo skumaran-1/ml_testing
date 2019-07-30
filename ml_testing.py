a6734bcca08d0f0d7c4ce391742011a7ee7d8fc4
Siddharth Kumaran <skumaran@rivasolutionsinc.com>
2019-07-30 10:50:31 -0400

    pre

--- ml_testing.py
+++ ml_testing.py
@@ -1,6 +1,9 @@
+import tensorflow as tf
 import numpy as np                                  #for large and multi-dimensional arrays
 import pandas as pd                                 #for data manipulation and analysis
 
-data = pd.read_csv(r'ITMO_raw_data.csv')
+from tensorflow.python.keras.preprocessing.text import text_to_word_sequence
 
-print(data)
+df = pd.read_csv(r'ITMO_raw_data.csv')
+
+print(df['Contract description:'].iloc[0])
