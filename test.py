b25285cfa3bd2819caa5072e8152eb191ace3157
Siddharth Kumaran <skumaran@rivasolutionsinc.com>
2019-08-19 10:45:29 -0400

    comments

--- test.py
+++ test.py
@@ -87,17 +87,18 @@ def stem(words):
     stems = stem_words(words)
     return stems
 ################################################################################
-
+# Load saved model from files
 model = load_model("model.h5")
-
+# prints models layers so we can see how many params
 model.summary()
-
+# load our dictionaries
 with open('dictionary.pickle', 'rb') as handle:
     dictionary = pickle.load(handle)
-
 with open('reverse_dictionary.pickle', 'rb') as handle:
     reverse_dictionary = pickle.load(handle)
-
+################################################################################
+# this depended on what file i was looking at
+#
 #df = pd.read_csv(r'0 contracts.csv')
 
 df = pd.read_csv(r'ITMO_raw_data.csv')
@@ -105,9 +106,9 @@ df = pd.read_csv(r'ITMO_raw_data.csv')
 description = df['Contract description']
 
 #description = df['tm_govsuite__description__c']
-
+################################################################################
 cleaned_descriptions = []
-
+# this loop word_tokenizes the text
 for i in range(len(df.index)):
     text = description.iloc[i]
     review_cleaned = replace_contractions(text)
@@ -117,22 +118,33 @@ for i in range(len(df.index)):
     cleaned_descriptions.append(review_cleaned)
 
 finalWordsNumberList = []
+# this loop is a little confusing to logically understand so lets break it down
+# first we need to turn words into their numbers, but what if a word already was
+# used in our old dictionary? that word would already be assigned a number
 for list in cleaned_descriptions:
     sentence = []
     for word in list:
         try:
+            # so we look up the word and replace it
             new = dictionary[word]
             word = new
             sentence.append(word)
         except:
+            # what if the word doesnt exist in our dictionary? well in our model
+            # if we try to give it a new word it vocab cap going to increased
+            # our model then freaks out and crash because it doesnt know how to
+            # interpret the new word/number
+            # work around for this is simply assigning it to 0
             word = 0
             sentence.append(word)
+    # add word to finalWordsNumberList like before
     finalWordsNumberList.append(sentence)
 
+# padding
 finalWordsNumberList = tf.keras.preprocessing.sequence.pad_sequences(finalWordsNumberList, value=0, padding='post')
 
 results = []
-
+# use the sentence predict based on the model and
 a = model.predict(finalWordsNumberList,use_multiprocessing=True)
-
+# results stored in a
 print(a)
